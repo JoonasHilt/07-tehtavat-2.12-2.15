d98f7c8da41c9f4a47d2eb61def8620e9401c3d7
JoonasHilt <joonas.hiltunen@hotmail.com>
2024-11-04 16:29:25 +0200

    alustettu repo

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Persons from "./components/Persons";
+import PersonForm from "./components/PersonForm";
+import Filter from "./components/Filter";
+
+const App = () => {
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    if (persons.some((person) => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`);
+      return;
+    }
+    setPersons(persons.concat({ name: newName, number: newNumber }));
+    setNewName("");
+    setNewNumber("");
+  };
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredPersons = persons.filter((person) => {
+    const firstName = person.name.split(" ")[0].toLowerCase();
+    const searchValue = searchTerm.toLowerCase();
+    return firstName.startsWith(searchValue);
+  });
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter searchTerm={searchTerm} handleSearchChange={handleSearchChange} />
+
+      <h3>Add a new</h3>
+      <PersonForm
+        addPerson={addPerson}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
+
+      <h3>Numbers</h3>
+      <Persons persons={filteredPersons} />
+    </div>
+  );
+};
+
+export default App;
