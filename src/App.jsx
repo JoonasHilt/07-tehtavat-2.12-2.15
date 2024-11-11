915956f90a18c5396201e38442ecd2ec1ab21980
JoonasHilt <joonas.hiltunen@hotmail.com>
2024-11-11 13:38:13 +0200

    tehtävä 2.15 step10 valmis

--- src/App.jsx
+++ src/App.jsx
@@ -18,8 +18,39 @@ const App = () => {
 
   const addPerson = (event) => {
     event.preventDefault();
-    if (persons.some((person) => person.name === newName)) {
-      alert(`${newName} is already added to phonebook`);
+
+    // Muunnetaan syötetty nimi pieniksi kirjaimiksi
+    const newNameLower = newName.toLowerCase();
+
+    // Tarkistetaan, onko nimeä jo olemassa riippumatta kirjainkoosta
+    const existingPerson = persons.find(
+      (person) => person.name.toLowerCase() === newNameLower
+    );
+
+    if (existingPerson) {
+      if (
+        window.confirm(
+          `${newName} is already added to phonebook, replace the old number with a new one?`
+        )
+      ) {
+        const updatedPerson = { ...existingPerson, number: newNumber };
+
+        addPersons
+          .update(existingPerson.id, updatedPerson)
+          .then((returnedPerson) => {
+            setPersons(
+              persons.map((person) =>
+                person.id !== existingPerson.id ? person : returnedPerson
+              )
+            );
+            setNewName("");
+            setNewNumber("");
+          })
+          .catch((error) => {
+            console.log("Error updating person:", error);
+            alert("An error occurred while updating the person");
+          });
+      }
       return;
     }
 
